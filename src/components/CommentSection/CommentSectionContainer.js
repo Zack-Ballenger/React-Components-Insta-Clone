788bb9370ded9588291e1f159e498ce865454753
Zack-Ballenger <zack.ballenger@tutanota.com>
2020-05-27 20:50:14 -0600

    Finally finished, updated the search bar and called dummy data correctly

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -1,18 +1,22 @@
+// You will add code to this file
 import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
 const CommentSection = props => {
+  // Add state for the comments
   return (
-    <div>
-      {props.comments.map(e => (
-        <Comment comment={e} />
-      ))}
-    <CommentInput currComments={props.currComments} updateComments={props.updateComments} comments={props.comments} 
-    />
-    </div>
-  );
+		<div>
+			{/* map through the comments data and return the Comment component */}
+			{props.comments.map((comment, index) => {
+				return <Comment key={`${comment.username}-${index}`} comment={comment} />;
+			})}
+      <CommentInput 
+        login={props.login}
+        comments={props.comments}
+        setComments={props.comments} />      
+		</div>
+	);
 };
-
 export default CommentSection;
